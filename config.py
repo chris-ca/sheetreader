a309c93c20cbf8927fe22b74cc78806ed0faa311
Chris Weyland <github@nextmail.cc>
2022-12-24 15:40:13 +0300

    test github actions

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+cache = {
+    "path": os.getenv("CACHE_DIR"),
+    "cache_time_gsheets": int(os.getenv("GSHEETS_CACHE")),
+}
+logbook = {
+    "key": "1SRUaIhL17R-mVhbJb-52xE7_GQtU5kEKhuRAaTHB3FY",
+    "auth_file": "service.json",
+}
